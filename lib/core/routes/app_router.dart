0d555b067e40b8679fccd372c2981331d54d6408
mayurshelar-coditas <mayur.shelar@coditas.com>
2024-06-22 15:21:52 +0530

    Pod File Error Resolved

--- lib/core/routes/app_router.dart
+++ lib/core/routes/app_router.dart
@@ -1,15 +1,13 @@
-
 import 'package:auto_route/auto_route.dart';
 import 'app_router.gr.dart';
 
 @AutoRouterConfig()
-class AppRouter extends $AppRouter
-{
+class AppRouter extends $AppRouter {
   @override
-  // TODO: implement routes
   List<AutoRoute> get routes => [
-    
-  ];
-
-
+        AutoRoute(
+          page: Home.page,
+          initial: true,
+        )
+      ];
 }
--- lib/core/routes/app_router.gr.dart
+++ lib/core/routes/app_router.gr.dart
@@ -8,11 +8,33 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:auto_route/auto_route.dart' as _i1;
+import 'package:auto_route/auto_route.dart' as _i2;
+import 'package:social_connect_app/features/home.dart' as _i1;
 
-abstract class $AppRouter extends _i1.RootStackRouter {
+abstract class $AppRouter extends _i2.RootStackRouter {
   $AppRouter({super.navigatorKey});
 
   @override
-  final Map<String, _i1.PageFactory> pagesMap = {};
+  final Map<String, _i2.PageFactory> pagesMap = {
+    Home.name: (routeData) {
+      return _i2.AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const _i1.Home(),
+      );
+    }
+  };
+}
+
+/// generated route for
+/// [_i1.Home]
+class Home extends _i2.PageRouteInfo<void> {
+  const Home({List<_i2.PageRouteInfo>? children})
+      : super(
+          Home.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'Home';
+
+  static const _i2.PageInfo<void> page = _i2.PageInfo<void>(name);
 }
