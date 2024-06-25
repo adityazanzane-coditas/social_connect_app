7b4f250dde24e41f18e25ca79c4bbd75a07165fc
yashwadatkar-coditas <yash.wadatkar@coditas.com>
2024-06-25 18:46:48 +0530

    implemented new feed feature

--- lib/core/navbar/navbar.dart
+++ lib/core/navbar/navbar.dart
@@ -2,7 +2,7 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:social_connect_app/core/routes/app_router.gr.dart';
-import 'package:social_connect_app/features/home/presentation/pages/home_screen.dart';
+
 
 @RoutePage()
 class Navbar extends StatelessWidget {
--- lib/core/routes/app_router.gr.dart
+++ lib/core/routes/app_router.gr.dart
@@ -8,72 +8,75 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:auto_route/auto_route.dart' as _i7;
+import 'package:auto_route/auto_route.dart' as _i8;
 import 'package:social_connect_app/core/navbar/navbar.dart' as _i3;
-import 'package:social_connect_app/features/onboarding/presentation/pages/onboarding_screen/presentation/pages/home_screen.dart'
-   
+import 'package:social_connect_app/features/home/presentation/pages/home_screen.dart'
     as _i1;
 import 'package:social_connect_app/features/messages/presentation/pages/messages.dart'
     as _i2;
-import 'package:social_connect_app/features/posts/presentation/pages/posts.dart'
+import 'package:social_connect_app/features/onboarding/presentation/pages/onboarding_screen.dart'
     as _i4;
-import 'package:social_connect_app/features/profile/presentation/pages/profile.dart'
+import 'package:social_connect_app/features/posts/presentation/pages/posts.dart'
     as _i5;
-import 'package:social_connect_app/features/setting/presentation/pages/setting.dart'
+import 'package:social_connect_app/features/profile/presentation/pages/profile.dart'
     as _i6;
+import 'package:social_connect_app/features/setting/presentation/pages/setting.dart'
+    as _i7;
 
-abstract class $AppRouter extends _i7.RootStackRouter {
+abstract class $AppRouter extends _i8.RootStackRouter {
   $AppRouter({super.navigatorKey});
 
   @override
-  final Map<String, _i7.PageFactory> pagesMap = {
+  final Map<String, _i8.PageFactory> pagesMap = {
     HomeRoute.name: (routeData) {
-      return _i7.AutoRoutePage<dynamic>(
+      return _i8.AutoRoutePage<dynamic>(
         routeData: routeData,
         child: const _i1.HomeScreen(),
       );
     },
     Messages.name: (routeData) {
-      return _i7.AutoRoutePage<dynamic>(
+      return _i8.AutoRoutePage<dynamic>(
         routeData: routeData,
         child: const _i2.Messages(),
       );
     },
     Navbar.name: (routeData) {
-      return _i7.AutoRoutePage<dynamic>(
+      return _i8.AutoRoutePage<dynamic>(
         routeData: routeData,
         child: const _i3.Navbar(),
       );
     },
-    PostsRoute.name: (routeData) {
-      return _i7.AutoRoutePage<dynamic>(
+    OnboardingRoute.name: (routeData) {
+      return _i8.AutoRoutePage<dynamic>(
         routeData: routeData,
-        child: const _i4.PostsScreen(),
+        child: const _i4.OnboardingScreen(),
+      );
+    },
+    PostsRoute.name: (routeData) {
+      return _i8.AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const _i5.PostsScreen(),
       );
     },
     Profile.name: (routeData) {
-      return _i7.AutoRoutePage<dynamic>(
+      return _i8.AutoRoutePage<dynamic>(
         routeData: routeData,
-        child: const _i5.Profile(),
+        child: const _i6.Profile(),
       );
     },
     Setting.name: (routeData) {
-      return _i7.AutoRoutePage<dynamic>(
+      return _i8.AutoRoutePage<dynamic>(
         routeData: routeData,
-        child: const _i1.OnboardingScreen(),
-        child: const _i6.Setting(),
+        child: const _i7.Setting(),
       );
     },
   };
 }
 
 /// generated route for
-/// [_i1.OnboardingScreen]
-class OnboardingRoute extends _i2.PageRouteInfo<void> {
-  const OnboardingRoute({List<_i2.PageRouteInfo>? children})
 /// [_i1.HomeScreen]
-class HomeRoute extends _i7.PageRouteInfo<void> {
-  const HomeRoute({List<_i7.PageRouteInfo>? children})
+class HomeRoute extends _i8.PageRouteInfo<void> {
+  const HomeRoute({List<_i8.PageRouteInfo>? children})
       : super(
           HomeRoute.name,
           initialChildren: children,
@@ -81,13 +84,13 @@ class HomeRoute extends _i7.PageRouteInfo<void> {
 
   static const String name = 'HomeRoute';
 
-  static const _i7.PageInfo<void> page = _i7.PageInfo<void>(name);
+  static const _i8.PageInfo<void> page = _i8.PageInfo<void>(name);
 }
 
 /// generated route for
 /// [_i2.Messages]
-class Messages extends _i7.PageRouteInfo<void> {
-  const Messages({List<_i7.PageRouteInfo>? children})
+class Messages extends _i8.PageRouteInfo<void> {
+  const Messages({List<_i8.PageRouteInfo>? children})
       : super(
           Messages.name,
           initialChildren: children,
@@ -95,13 +98,13 @@ class Messages extends _i7.PageRouteInfo<void> {
 
   static const String name = 'Messages';
 
-  static const _i7.PageInfo<void> page = _i7.PageInfo<void>(name);
+  static const _i8.PageInfo<void> page = _i8.PageInfo<void>(name);
 }
 
 /// generated route for
 /// [_i3.Navbar]
-class Navbar extends _i7.PageRouteInfo<void> {
-  const Navbar({List<_i7.PageRouteInfo>? children})
+class Navbar extends _i8.PageRouteInfo<void> {
+  const Navbar({List<_i8.PageRouteInfo>? children})
       : super(
           Navbar.name,
           initialChildren: children,
@@ -109,13 +112,27 @@ class Navbar extends _i7.PageRouteInfo<void> {
 
   static const String name = 'Navbar';
 
-  static const _i7.PageInfo<void> page = _i7.PageInfo<void>(name);
+  static const _i8.PageInfo<void> page = _i8.PageInfo<void>(name);
 }
 
 /// generated route for
-/// [_i4.PostsScreen]
-class PostsRoute extends _i7.PageRouteInfo<void> {
-  const PostsRoute({List<_i7.PageRouteInfo>? children})
+/// [_i4.OnboardingScreen]
+class OnboardingRoute extends _i8.PageRouteInfo<void> {
+  const OnboardingRoute({List<_i8.PageRouteInfo>? children})
+      : super(
+          OnboardingRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'OnboardingRoute';
+
+  static const _i8.PageInfo<void> page = _i8.PageInfo<void>(name);
+}
+
+/// generated route for
+/// [_i5.PostsScreen]
+class PostsRoute extends _i8.PageRouteInfo<void> {
+  const PostsRoute({List<_i8.PageRouteInfo>? children})
       : super(
           PostsRoute.name,
           initialChildren: children,
@@ -123,13 +140,13 @@ class PostsRoute extends _i7.PageRouteInfo<void> {
 
   static const String name = 'PostsRoute';
 
-  static const _i7.PageInfo<void> page = _i7.PageInfo<void>(name);
+  static const _i8.PageInfo<void> page = _i8.PageInfo<void>(name);
 }
 
 /// generated route for
-/// [_i5.Profile]
-class Profile extends _i7.PageRouteInfo<void> {
-  const Profile({List<_i7.PageRouteInfo>? children})
+/// [_i6.Profile]
+class Profile extends _i8.PageRouteInfo<void> {
+  const Profile({List<_i8.PageRouteInfo>? children})
       : super(
           Profile.name,
           initialChildren: children,
@@ -137,21 +154,19 @@ class Profile extends _i7.PageRouteInfo<void> {
 
   static const String name = 'Profile';
 
-  static const _i7.PageInfo<void> page = _i7.PageInfo<void>(name);
+  static const _i8.PageInfo<void> page = _i8.PageInfo<void>(name);
 }
 
 /// generated route for
-/// [_i6.Setting]
-class Setting extends _i7.PageRouteInfo<void> {
-  const Setting({List<_i7.PageRouteInfo>? children})
+/// [_i7.Setting]
+class Setting extends _i8.PageRouteInfo<void> {
+  const Setting({List<_i8.PageRouteInfo>? children})
       : super(
-          OnboardingRoute.name,
           Setting.name,
           initialChildren: children,
         );
 
-  static const String name = 'OnboardingRoute';
   static const String name = 'Setting';
 
-  static const _i7.PageInfo<void> page = _i7.PageInfo<void>(name);
+  static const _i8.PageInfo<void> page = _i8.PageInfo<void>(name);
 }
